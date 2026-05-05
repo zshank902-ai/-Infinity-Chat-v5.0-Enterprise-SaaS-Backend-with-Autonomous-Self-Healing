d87e0fbdc7e49c05ddab786ac4ef32648c50f702
Zeesh <zshank902@gmail.com>
2026-05-05 09:24:47 +0530

    UI Fix: Allow HuggingFace framing by setting X-Frame-Options to ALLOWALL

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ async def security_middleware(request: Request, call_next):
     # Security Headers
     response = await call_next(request)
     response.headers["X-Content-Type-Options"] = "nosniff"
-    response.headers["X-Frame-Options"] = "DENY"
+    response.headers["X-Frame-Options"] = "ALLOWALL"
     return response
 
 class ChatRequest(BaseModel):
